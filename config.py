cc858e18387cd424b8575487a49721ff2d5b9ecc
dhanush2bot <149936819+dhanush2bot@users.noreply.github.com>
2023-12-30 21:44:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,22 @@
-#(©)CodeXBotz
-
-
-
-
 import os
+from datetime import datetime, timedelta
 import logging
 from logging.handlers import RotatingFileHandler
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
 
+# Log level for logging
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+
+# Premium user IDs
+PREMIUM_USERS = []
+
+# Owner ID
+OWNER_ID = int(os.getenv("OWNER_ID", "1764208280"))
+
+# Other configurations...
 
 #Bot token @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
@@ -20,9 +29,6 @@ API_HASH = os.environ.get("API_HASH", "")
 
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
-
-#OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
@@ -38,35 +44,13 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
-try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
-        ADMINS.append(int(x))
-except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
 
-#Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+# ...
 
-#set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
-
-#set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
-
-#Set true if you want Disable your Channel Posts Share button
-DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
-
-BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
-
-ADMINS.append(OWNER_ID)
-ADMINS.append(1764208280)
-
+# Logging configuration
 LOG_FILE_NAME = "filesharingbot.txt"
-
 logging.basicConfig(
-    level=logging.INFO,
+    level=LOG_LEVEL,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt='%d-%b-%y %H:%M:%S',
     handlers=[
@@ -83,3 +67,24 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
+
+
+# Function to add a user to premium
+def add_premium_user(user_id, days):
+    expiry_time = datetime.now() + timedelta(days=days)
+    PREMIUM_USERS.append({"user_id": user_id, "expiry_time": expiry_time})
+    notify_user(user_id, expiry_time)
+
+
+# Function to notify the user about premium status
+def notify_user(user_id, expiry_time):
+    # Add your notification logic here
+    pass
+
+
+# Function to check if a user is premium
+def is_premium_user(user_id):
+    for user in PREMIUM_USERS:
+        if user["user_id"] == user_id:
+            return datetime.now() < user["expiry_time"]
+    return False
